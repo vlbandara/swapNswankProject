a3011d08695cb333ad4132ad9366638f101ed308
vlbandara <137267283+vlbandara@users.noreply.github.com>
2024-05-11 21:39:59 +0530

    commons added

---
+++ profile-service/src/main/java/com/aggrandizer/swapNswank/profileservice/controller/ProfileController.java
@@ -0,0 +1,12 @@
+package com.aggrandizer.swapNswank.profileservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/services")
+public class ProfileController {
+
+
+
+}
